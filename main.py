94b0b25019b236f70e0d5c3fc9e43cae8d2d7877
agente-perry <hackaton942@gmail.com>
2026-05-17 22:11:12 -0500

    fix CORS for Vercel

--- main.py
+++ main.py
@@ -20,7 +20,12 @@ app = FastAPI(title="Perry Agent API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://agente-perry-1ndr.vercel.app",
+        "https://*.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
